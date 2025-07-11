4f9478b05e4b0023432ffaa89f3a8f09f96eafc7
marcelaromas <marcelapopular2011@hotmail.com>
2025-07-11 01:33:34 -0300

    fix: configuración Firebase hardcodeada final

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,15 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyDkqkoiJohNtZlPMRVqwBTbttAK7Kee4zo",
+  authDomain: "pepshop-8cc16.firebaseapp.com",
+  projectId: "pepshop-8cc16",
+  storageBucket: "pepshop-8cc16.appspot.com",
+  messagingSenderId: "580462653305",
+  appId: "1:580462653305:web:1e1dc6f1392fbddb6349a8",
 };
-
-console.log("🔥 ENV desde Vercel:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
